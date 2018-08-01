4967d90a3f4856022d3276778226f2052c30a744
kenttc <kenttc@gmail.com>
2018-08-01 20:18:03 +0100

    removed read - didn't seem to be used anywhere - and then now taking a 30min break

--- src/AddressProcessor/CSV/CSVReaderWriter.cs
+++ src/AddressProcessor/CSV/CSVReaderWriter.cs
@@ -63,36 +63,6 @@ namespace AddressProcessing.CSV
             WriteLine(outPut);
         }
 
-        public bool Read(string column1, string column2)
-        {
-            const int FIRST_COLUMN = 0;
-            const int SECOND_COLUMN = 1;
-
-            string line;
-            string[] columns;
-
-            char[] separator = { '\t' };
-
-            line = ReadLine();
-
-            columns = line.Split(separator);
-
-            if (columns.Length == 0)
-            {
-                column1 = null;
-                column2 = null;
-
-                return false;
-            }
-            else
-            {
-                column1 = columns[FIRST_COLUMN];
-                column2 = columns[SECOND_COLUMN];
-
-                return true;
-            }
-        }
-
         public bool Read(out string column1, out string column2)
         {
             const int FIRST_COLUMN = 0;
