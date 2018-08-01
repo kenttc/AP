ffa3efa39e1dc58aaf437a70b804bed1ff6f260d
kenttc <kenttc@gmail.com>
2018-08-01 18:54:16 +0100

    first commit with the annotations in there

---
+++ src/AddressProcessor/CSV/CSVReaderWriter For Annotation.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+
+namespace AddressProcessing.CSV
+{
+    /*
+        1) List three to five key concerns with this implementation that you would discuss with the junior developer. 
+
+        Please leave the rest of this file as it is so we can discuss your concerns during the next stage of the interview process.
+        
+        *)const can be refactored out to the top of the class so that it doesn't get repeated. 
+        *)where possible use gated if - so that we don't really need to use else
+        *)I would put extract the static File class to implement an IFile so that it can be injected during construction so it's decoupled
+        *)we could implement Idisposable for this class so the closing is more standardised
+        *)output could use stringbuilder as it's mutable instead of string + which is immutable 
+        * 
+    */
+
+    public class CSVReaderWriterForAnnotation
+    {
+        private StreamReader _readerStream = null;
+        private StreamWriter _writerStream = null;
+
+        [Flags]
+        public enum Mode { Read = 1, Write = 2 };
+
+        public void Open(string fileName, Mode mode)
+        {
+            if (mode == Mode.Read)
+            {
+                _readerStream = File.OpenText(fileName);
+            }
+            else if (mode == Mode.Write)
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                _writerStream = fileInfo.CreateText();
+            }
+            else
+            {
+                throw new Exception("Unknown file mode for " + fileName);
+            }
+        }
+
+        public void Write(params string[] columns)
+        {
+            string outPut = "";
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                outPut += columns[i];
+                if ((columns.Length - 1) != i)
+                {
+                    outPut += "\t";
+                }
+            }
+
+            WriteLine(outPut);
+        }
+
+        public bool Read(string column1, string column2)
+        {
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            string line;
+            string[] columns;
+
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+            columns = line.Split(separator);
+
+            if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+            else
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+        }
+
+        public bool Read(out string column1, out string column2)
+        {
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            string line;
+            string[] columns;
+
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+
+            if (line == null)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+
+            columns = line.Split(separator);
+
+            if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            } 
+            else
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+        }
+
+        private void WriteLine(string line)
+        {
+            _writerStream.WriteLine(line);
+        }
+
+        private string ReadLine()
+        {
+            return _readerStream.ReadLine();
+        }
+
+        public void Close()
+        {
+            if (_writerStream != null)
+            {
+                _writerStream.Close();
+                
+            }
+
+            if (_readerStream != null)
+            {
+                _readerStream.Close();
+            }
+        }
+    }
+}
